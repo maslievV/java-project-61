f1c861836e2a6c0bead8836a113a8c64d55a3020
Владислав <maslievvlad8@gmail.com>
2024-09-05 20:41:15 +0300

    add content

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+    id("application")
+    id("jacoco")
+}
+
+application { mainClass.set("hexlet.code.App") }
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
