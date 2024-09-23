14a8cfd471acd1715fca3f4196dccbbfb88402fc
Владислав <maslievvlad8@gmail.com>
2024-09-23 18:30:16 +0300

    fix linter

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -2,8 +2,12 @@ package hexlet.code;
 
 
 public class Utils {
+    private static final int RANDOM_SIZE = 100;
+
     public static int makeRandomNumber(int num) {
         return (int) (Math.random() * num);
     }
-    public static int makeRandomNumber() { return (int) (Math.random() * 100); }
+    public static int makeRandomNumber() {
+        return (int) (Math.random() * RANDOM_SIZE);
+    }
 }
